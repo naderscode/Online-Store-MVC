74eaa8b9542cfa82a7d1c39c209d81c3ac5f21fa
naderscode <naderscode@gmail.com>
2018-05-25 23:24:14 -0700

    update SeedData.cs

--- SportsStore/SportsStore/Models/SeedData.cs
+++ SportsStore/SportsStore/Models/SeedData.cs
@@ -1,91 +1,152 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
 using Microsoft.AspNetCore.Builder;
+
 using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.EntityFrameworkCore;
+
+
 
 namespace SportsStore.Models
 {
-    public static class SeedData {
 
-        public static void EnsurePopulated(IApplicationBuilder app) {
+
+
+    public static class SeedData
+    {
+
+
+
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+
             ApplicationDbContext context = app.ApplicationServices
+
                 .GetRequiredService<ApplicationDbContext>();
+
             context.Database.Migrate();
-            if (!context.Products.Any()) {
+
+            if (!context.Products.Any())
+            {
+
                 context.Products.AddRange(
-                    new Product {
+
+                    new Product
+                    {
+
                         Name = "Kayak",
                         Description = "A boat for one person",
+
                         Category = "Watersports",
-                        Price = 270
+                        Price = 275
+
                     },
 
                     new Product
                     {
-                        Name = "Life Jacket",
-                        Description = "Protective with Fashion",
+
+                        Name = "Lifejacket",
+
+                        Description = "Protective and fashionable",
+
                         Category = "Watersports",
-                        Price =49.00m
-                    },
-                     new Product
-                     {
-                         Name = "Soccer Ball",
-                         Description = "FIFA-approved size and weight",
-                         Category = "Soccer",
-                         Price =25m
+                        Price = 48.95m
+
                     },
 
-                     new Product
-                     {
-                         Name = "Corner Flags",
-                         Description = "To add a professional touch",
-                         Category = "Soccer",
-                         Price =35.99m
+                    new Product
+                    {
+
+                        Name = "Soccer Ball",
+
+                        Description = "FIFA-approved size and weight",
+
+                        Category = "Soccer",
+                        Price = 19.50m
+
                     },
-                     new Product
-                     {
-                         Name = "Stadium",
-                         Description = "Flat-packed 35,000 -seat stadium",
-                         Category = "Soccer",
-                         Price =80,000
+
+                    new Product
+                    {
+
+                        Name = "Corner Flags",
+
+                        Description = "Give your playing field a professional touch",
+
+                        Category = "Soccer",
+                        Price = 34.95m
+
                     },
-                     new Product
-                     {
-                         Name = "Thinking Cap",
-                         Description = "Improves brain efficiency",
-                         Category = "Chess",
-                         Price =19
+
+                    new Product
+                    {
+
+                        Name = "Stadium",
+
+                        Description = "Flat-packed 35,000-seat stadium",
+
+                        Category = "Soccer",
+                        Price = 79500
+
                     },
-                     new Product
-                     {
-                         Name = "Unsteady Chair",
-                         Description = "Give your oppenent a disadvantage",
-                         Category = "Chess",
-                         Price =35.00m
+
+                    new Product
+                    {
+
+                        Name = "Thinking Cap",
+
+                        Description = "Improve brain efficiency by 75%",
+
+                        Category = "Chess",
+                        Price = 16
+
                     },
-                     new Product
-                     {
-                         Name = "Human Chess Board",
-                         Description = "A fun game for the family",
-                         Category = "Chess",
-                         Price =75
+
+                    new Product
+                    {
+
+                        Name = "Unsteady Chair",
+
+                        Description = "Secretly give your opponent a disadvantage",
+
+                        Category = "Chess",
+                        Price = 29.95m
+
                     },
-                     new Product
-                     {
-                         Name = "Bling-Bling King",
-                         Description = "Gold plated King",
-                         Category = "Chess",
-                         Price =900
+
+                    new Product
+                    {
+
+                        Name = "Human Chess Board",
+
+                        Description = "A fun game for the family",
+
+                        Category = "Chess",
+                        Price = 75
+
+                    },
+
+                    new Product
+                    {
+
+                        Name = "Bling-Bling King",
+
+                        Description = "Gold-plated, diamond-studded King",
+
+                        Category = "Chess",
+                        Price = 1200
+
                     }
 
-                 );
+                );
+
                 context.SaveChanges();
 
             }
+
         }
 
     }
-}
+
+}   
