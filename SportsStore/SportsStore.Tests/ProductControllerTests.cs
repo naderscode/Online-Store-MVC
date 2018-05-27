96fb3cbf2bb51fff9af8a466f21bb73317c10122
naderscode <naderscode@gmail.com>
2018-05-26 17:41:36 -0700

    update product controller test file

--- SportsStore/SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -10,7 +10,7 @@ using SportsStore.Models;
 
 using Xunit;
 
-//using SportsStore.Models.ViewModels;
+using SportsStore.Models.ViewModels;
 
 
 
@@ -48,6 +48,7 @@ namespace SportsStore.Tests
             }).AsQueryable<Product>());
 
 
+
             ProductController controller = new ProductController(mock.Object);
 
             controller.PageSize = 3;
@@ -56,15 +57,15 @@ namespace SportsStore.Tests
 
             // Act
 
-            IEnumerable<Product> result =
+            ProductsListViewModel result =
 
-                controller.List(2).ViewData.Model as IEnumerable<Product> ;
+                controller.List(2).ViewData.Model as ProductsListViewModel;
 
 
 
             // Assert
 
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
 
             Assert.True(prodArray.Length == 2);
 
@@ -73,5 +74,70 @@ namespace SportsStore.Tests
             Assert.Equal("P5", prodArray[1].Name);
 
         }
+
+
+
+
+
+        [Fact]
+
+        public void Can_Send_Pagination_View_Model()
+        {
+
+
+
+            // Arrange
+
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns((new Product[] {
+
+                new Product {ProductID = 1, Name = "P1"},
+
+                new Product {ProductID = 2, Name = "P2"},
+
+                new Product {ProductID = 3, Name = "P3"},
+
+                new Product {ProductID = 4, Name = "P4"},
+
+                new Product {ProductID = 5, Name = "P5"}
+
+            }).AsQueryable<Product>());
+
+
+
+            // Arrange
+
+            ProductController controller =
+
+                new ProductController(mock.Object) { PageSize = 3 };
+
+
+
+            // Act
+
+            ProductsListViewModel result =
+
+                controller.List(2).ViewData.Model as ProductsListViewModel;
+
+
+
+            // Assert
+
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.Equal(2, pageInfo.CurrentPage);
+
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+
+            Assert.Equal(5, pageInfo.TotalItems);
+
+            Assert.Equal(2, pageInfo.TotalPages);
+
+        }
+
+
+
     }
+
 }
