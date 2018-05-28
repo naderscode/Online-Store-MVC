45a9c2eae432b19bc0ffe38c06ecca84829f86b5
naderscode <naderscode@gmail.com>
2018-05-27 21:49:57 -0700

    update List method in ProductController.cs file

--- SportsStore/SportsStore/Controllers/ProductController.cs
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers { 
 
@@ -15,10 +16,20 @@ namespace SportsStore.Controllers {
             repository = repo;
         }
 
-        public ViewResult List(int productPage=1)
-            => View(repository.Products
-                .OrderBy(p=>p.ProductID)
-                .Skip((productPage-1*PageSize)
-                .Take(PageSize));
+        public ViewResult List(int productPage = 1)
+            => View(new ProductListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+
+            });
     }
 }
