5496f5bd94a9a007c64fa79713c19b620d18dcf2
naderscode <naderscode@gmail.com>
2018-06-01 14:30:55 -0700

    create NavigationMenuViewComponent.cs file

---
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent:ViewComponent
+    {
+        public string Invoke() {
+            return "Nav View Component";
+        }
+    }
+}
