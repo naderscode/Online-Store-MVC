f021436855c1757318b263e835f3b4b2e0f7fc5e
naderscode <naderscode@gmail.com>
2018-06-02 17:42:09 -0700

    add categories in the NavigationMenuViewComponent.cs

--- SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Components
 {
     public class NavigationMenuViewComponent:ViewComponent
     {
-        public string Invoke() {
-            return "Nav View Component";
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo) {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke() {
+            return View(repository.Products
+                .Select(x=> x.Category)
+                .Distinct()
+                .OrderBy(x=>x));
         }
     }
 }
